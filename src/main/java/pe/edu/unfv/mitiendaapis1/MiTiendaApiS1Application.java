b3266dc9e6a7416eed31f3b10ee83c68b2248e79
Club Fitnes 2020 <clubfitnes2020@gmail.com>
2024-06-14 01:41:51 -0500

    first commit

---
+++ src/main/java/pe/edu/unfv/mitiendaapis1/MiTiendaApiS1Application.java
@@ -0,0 +1,13 @@
+package pe.edu.unfv.mitiendaapis1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiTiendaApiS1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiTiendaApiS1Application.class, args);
+	}
+
+}
